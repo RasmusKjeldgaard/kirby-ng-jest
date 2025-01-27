ce9210ea20e4341d702e8b04ac474f73eac65115
RasmusKjeldgaard <rkk@bankdata.dk>
2025-01-27 19:14:28 +0100

    Angular 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  CheckboxComponent,
+  HeaderModule,
+  IconModule,
+  ItemModule,
+  PageModule,
+} from '@kirbydesign/designsystem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [
+    PageModule,
+    ItemModule,
+    IconModule,
+    CheckboxComponent,
+    HeaderModule,
+  ],
+  standalone: true,
 })
 export class AppComponent {
-  title = 'ng-17';
+  title = 'ng-18';
 }
